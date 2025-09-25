393d52b614a729a91ae35e53ab2013b01cbd7c03
Mack Prda <mprda2@huskers.unl.edu>
2025-09-25 12:50:09 -0500

    added raegans major

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 int main(void) {
 
-printf("Mack A Prda \n");
-printf("Computer Science.");
-return 0;
+    printf("Mack A Prda \n");
+    printf("Computer Science.\n");
+    printf("Raegan Scheet\n");
+    printf("Software Engineering\n");
 
+    return 0;
 }
